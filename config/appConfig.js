9d0c0e368a01399fb96872b989efa27f4b104790
SARTHAK DAS <sarthakdas1608@gmail.com>
2019-07-22 00:54:50 +0530

    first commit

---
+++ config/appConfig.js
@@ -0,0 +1,19 @@
+let appConfig = {};
+
+appConfig.port = process.env.port || 8081; //3000;
+appConfig.allowedCorsOrigin = "*";
+appConfig.env = "dev";
+appConfig.db = {
+  //uri: "mongodb://127.0.0.1:27017/MeetingPlannerDB"
+  uri: 'mongodb+srv://sarthakJgd:brye6QzRFulKmueh@cluster0-wrg5n.mongodb.net/MeetingPlannerDB?retryWrites=true',
+  
+};
+appConfig.apiVersion = "/api/v1";
+
+module.exports = {
+  port: appConfig.port,
+  allowedCorsOrigin: appConfig.allowedCorsOrigin,
+  environment: appConfig.env,
+  db: appConfig.db,
+  apiVersion: appConfig.apiVersion
+};
